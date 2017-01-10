6ddc7abe03ce55caa684d8cf4dc3c67074fd34f7
paintception <matthia.sabatelli@gmail.com>
2017-01-10 14:47:42 +0100

    bitmap representation for every piece, every square

--- pgnbitmapper.py
+++ pgnbitmapper.py
@@ -1,25 +1,44 @@
 import chess.pgn
+import time
 
-SQUARES_180 = [
-    A8, B8, C8, D8, E8, F8, G8, H8,
-    A7, B7, C7, D7, E7, F7, G7, H7,
-    A6, B6, C6, D6, E6, F6, G6, H6,
-    A5, B5, C5, D5, E5, F5, G5, H5,
-    A4, B4, C4, D4, E4, F4, G4, H4,
-    A3, B3, C3, D3, E3, F3, G3, H3,
-    A2, B2, C2, D2, E2, F2, G2, H2,
-    A1, B1, C1, D1, E1, F1, G1, H1] = range(64)
+P_input_vec = []
+R_input_vec = []
+N_input_vec = []
+B_input_vec = []
+Q_input_vec = []
+K_input_vec = []
 
+p_input_vec = []
+r_input_vec = []
+n_input_vec = []
+b_input_vec = []
+q_input_vec = []
+k_input_vec = []
 
-inp = open('Games/Zysk, Robert-Postny, Evgeny.pgn')
+inp = open('Games/Lobo, Richard-Cusi, Ronald.pgn')
 
 game = chess.pgn.read_game(inp)
 
 node = game 
-while node.variations:
-    next_node = node.variation(0)
+
+while node.variations:  #For every node in the pgn extracts bitmap representation for every piece type
     
-    for i in SQUARES_180:
-        print (node.board().piece_at(i))    #Add different Piece representation + zeros and ones
-    print('\n')
+    next_node = node.variation(0)
+
+    P_input_vec.append(node.board().pieces(chess.PAWN, chess.WHITE))
+    R_input_vec.append(node.board().pieces(chess.ROOK, chess.WHITE))
+    N_input_vec.append(node.board().pieces(chess.KNIGHT, chess.WHITE))
+    B_input_vec.append(node.board().pieces(chess.BISHOP, chess.WHITE))
+    Q_input_vec.append(node.board().pieces(chess.QUEEN, chess.WHITE))
+    K_input_vec.append(node.board().pieces(chess.KING, chess.WHITE))
+
+    p_input_vec.append(node.board().pieces(chess.PAWN, chess.BLACK))
+    r_input_vec.append(node.board().pieces(chess.ROOK, chess.BLACK))
+    n_input_vec.append(node.board().pieces(chess.KNIGHT, chess.BLACK))
+    b_input_vec.append(node.board().pieces(chess.BISHOP, chess.BLACK))
+    q_input_vec.append(node.board().pieces(chess.QUEEN, chess.BLACK))
+    k_input_vec.append(node.board().pieces(chess.KING, chess.BLACK))
+
+    time.sleep(0.2)
+    
     node = next_node
