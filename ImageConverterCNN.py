63ff3d93dcd409543c3a265fa726861a55d7a363
paintception <matthia.sabatelli@gmail.com>
2017-03-19 12:09:17 +0100

    more efficient image maker Conv Net

--- ImageConverterCNN.py
+++ ImageConverterCNN.py
@@ -1,13 +1,16 @@
-#Remember to maked the dirs manually
-
 import os
 import numpy as np
 import cv2
 
+from sklearn import preprocessing
+
 def write_pic(data, path, name):
 	
 	tmp=np.asarray(data, dtype=np.uint8)
-	cv2.imwrite(path+name+'.jpg', tmp)
+	tmp = cv2.resize(tmp, (64, 64))	#Noise added to get bigger pictures
+	cv2.imwrite(path+name+'.png', tmp)
+
+	print "Image Saved"
 
 def do_path(path):
 	try:
@@ -26,23 +29,14 @@ def fix_pic(data):
 
 def main():
 
+	print "Reading the data"
 
-	X = []
-	y = []
+	X = np.load('/home/matthia/Desktop/MSc.-Thesis/Datasets/Numpy/Positions.npy')
+	y = np.load('/home/matthia/Desktop/MSc.-Thesis/Datasets/Numpy/Labels.npy')
+
+	X = preprocessing.scale(X)
+
 	General_X = []
-	ind=1
-
-	with open("FicsLabelled.txt", 'r') as f:
-		for line in f:
-			record = line.split(";")
-			pieces = [eval(x) for x in record[0:12]]
-			piece = [item for sublist in pieces for item in sublist]
-			piece = [item for sublist in piece for item in sublist]	
-
-			X.append(piece)
-			y.append(float(record[12][:-2]))
-
-	X = np.asarray(X)
 
 	for i in X:
 		g = i.reshape((12,64))
@@ -53,31 +47,51 @@ def main():
 
 	General_X = np.asarray(General_X)
 
-	path='/Users/matthiasabatelli/Desktop/MSc.-Thesis/CNNImages/'
+	path='/home/matthia/Desktop/MSc.-Thesis/CNNImages/BasicEvaluation/'
 	
 	do_path(path)
 
-	for ind, pos in enumerate(General_X[:2000]):
-		evalation = y[ind]
+	for ind, pos in enumerate(General_X):
+		
+		print "Making Images"
+
+		evaluation = y[ind]
 		pos=fix_pic(pos)
 		
-		if evalation >= - 0.5 and evalation < 0.5:
+		if evaluation == "Equal":
 			tmp_path=path+"Equal/"
-		elif evalation > 0.5 and evalation <= 1.5:
-			tmp_path=path+"WSB/"
-		elif evalation > 1.5 and evalation <= 4:
-			tmp_path=path+"WWB/"
-		elif evalation >4:
+		elif evaluation == "WW":
 			tmp_path=path+"WW/"
-		elif evalation < -0.5 and evalation >= - 1.5:
-			tmp_path=path+"BSB/"
-		elif evalation < 1.5 and evalation >= -4:
-			tmp_path=path+"BWB/"
-		elif evalation < -4:
+		elif evaluation == "BW":
 			tmp_path=path+"BW/"
-
-		write_pic(pos, tmp_path, 'pic'+str(ind))
-
+		
+		write_pic(pos, tmp_path, 'position'+str(ind))
+		
+		"""
+		if evaluation >= - 1 and evaluation <=1:
+			tmp_path=path+"Equal/"
+		elif evaluation > 1:
+			tmp_path=path+"WW/"
+		elif evaluation < 1:
+			tmp_path=path+"BW/"
+		"""
+		#write_pic(pos, tmp_path, 'position'+str(ind))
+		"""
+		if evaluation >= - 0.5 and evaluation < 0.5:
+			tmp_path=path+"Equal/"
+		elif evaluation > 0.5 and evaluation <= 1.5:
+			tmp_path=path+"WSB/"
+		elif evaluation > 1.5 and evaluation <= 4:
+			tmp_path=path+"WWB/"
+		elif evaluation >4:
+			tmp_path=path+"WW/"
+		elif evaluation < -0.5 and evaluation >= - 1.5:
+			tmp_path=path+"BSB/"
+		elif evaluation < 1.5 and evaluation >= -4:
+			tmp_path=path+"BWB/"
+		elif evaluation < -4:
+			tmp_path=path+"BW/"
+		"""
 if __name__ == '__main__':
 	main()
 	
